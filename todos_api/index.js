1ae91398d6ee0a23b26ba46ec78abe5c6614d86c
ub3rpyr0 <scott.wenzl@gmail.com>
2018-03-11 18:54:42 -0700

    update index.js to use port var instead of hard-coding

--- todos_api/index.js
+++ todos_api/index.js
@@ -1,10 +1,11 @@
 var express = require("express"),
-	app = express();
+	app = express(),
+	port = 3000;
 
 app.get("/", function(req, res){
 	res.send("Hello from Express");
 });
 
-app.listen(3000, function(){
-	console.log("App is running on port 3000");
+app.listen(port, function(){
+	console.log("App is running on port", port);
 });
